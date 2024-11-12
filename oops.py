60fcb2ac82f0d21a50120c11614a41939ec58f07
sriman3 <srimansakthi8@gmail.com>
2024-11-12 12:39:18 +0530

    python code

---
+++ oops.py
@@ -0,0 +1,14 @@
+class Iit():
+    def ccblock(self):
+    print("p.g senapathy center for computing resources")
+    
+class Gate():
+    def velachery(self):
+    print("2nd main gate")
+    
+iit=Iit()
+iit.ccblock()
+
+og=Gate()
+og.velachery()
+
